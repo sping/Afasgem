f168d5e88e856d47798fbc1c31180f359063bf07
Robert Bakker <r.bakker@sping.nl>
2019-02-21 20:45:38 +0100

    cleanup

--- lib/afasgem/baseconnector.rb
+++ lib/afasgem/baseconnector.rb
@@ -2,7 +2,7 @@ class BaseConnector
   
   def get_client(connector_url)
     client_params = {wsdl: connector_url}
-    if Afasgem::debug || !Afasgem::payload_logger.nil?
+    if Afasgem::debug || Afasgem::payload_logger
       client_params[:log] = true
       client_params[:log_level] = :debug
       client_params[:pretty_print_xml] = true
